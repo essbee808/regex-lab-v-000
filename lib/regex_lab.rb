305f3955133db2600cb25ef70638dde68a6e299e
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-14 23:19:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if "#{text}".match(/^[A-Z][a-z]{1,}|[A-Z]/) && "#{text}".match(/[!.?]$/)
     return true
-  elsif "#{text}".match(/^[a-z][a-z]{1,}|\b[a-z]\b/)
+  elsif "#{text}".match(/^[a-z][a-z]{1,}|\b[a-z]\b/) && "#{text}".match(/[!.?]$/)
     return false
   end
 end
